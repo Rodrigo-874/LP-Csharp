7b0e34a7b42e2d303c9cf1afab5cb58b5ef661b2
Rodrigo-874 <mocrodrigo414@gmail.com>
2026-06-15 13:00:55 +0100

    Add files via upload

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+public class Exercicio10
+{
+    public static void Main(string [] args)
+    {
+        int soma = 0;
+
+        for (int contador = 0; contador <= 50; contador++)
+        {
+            if(contador %2 != 0)
+            {
+                soma += contador;
+            }
+            
+        }
+        Console.WriteLine(soma);
+    }      
+}
