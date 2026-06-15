a410e6b24e81ed37c3914a7f90591ed87bbafaf4
Rodrigo-874 <mocrodrigo414@gmail.com>
2026-06-15 10:21:20 +0100

    Add files via upload

---
+++ Exercicio 8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+public class Exercicio_8
+{
+    public static void Main(string[] args)
+    {
+        int nota = 19;
+        switch (nota)
+        {
+            case int n when (nota > 20 || nota < 0):
+            Console.WriteLine("Nota invalida");
+                break;
+            case int n when (nota >= 17):
+                Console.WriteLine("Excelente");
+                break;
+
+            case int n when (nota >= 14):
+                Console.WriteLine("Bom");
+                break;
+
+            case int n when (nota >= 12):
+                Console.WriteLine("Suficiente");
+                break;
+
+            case int n when (nota >= 7):
+                Console.WriteLine("Insuficiente");
+                break;
+
+            default:
+                Console.WriteLine("Nota invalida");
+                break;
+
+        }
+    }
+}
