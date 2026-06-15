8d75632b332b7148498f82f4619c3ca4abff3c91
Rodrigo-874 <mocrodrigo414@gmail.com>
2026-06-15 10:25:19 +0100

    Add files via upload

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Exercicio5
+{
+    public static void Main(string[] args)
+    {
+        int idade = 24;
+        bool casado = true;
+
+        if (idade < 18)
+        {
+            Console.WriteLine("És menor de idade.");
+        }
+        if (idade >= 18)
+        {
+              if (casado)
+            {
+                Console.WriteLine("És adulto e casado.");
+            }
+              else
+            {
+                Console.WriteLine("És adulto e solteiro.");
+            }
+        }
+        else if (idade >= 65)
+        {
+            Console.WriteLine("És sénior.");
+        }
+
+    }
+}
