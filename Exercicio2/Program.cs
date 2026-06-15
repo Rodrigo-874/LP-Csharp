a2861a11fed34cc1f3e9e4d6e8cc7b579079df94
Rodrigo-874 <mocrodrigo414@gmail.com>
2026-06-15 10:26:55 +0100

    Add files via upload

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+    public class Exercicio2
+{ 
+    public static void Main(string[]args)
+    {
+       double raio = 7;
+       const double PI = 3.14;
+
+       double area = raio * raio * PI;
+
+       Console.WriteLine("O raio é: " + raio);
+       Console.WriteLine("A área do circulo é: " + area);
+
+
+    }
+        
+        
+}
