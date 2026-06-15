771b3aaa397410f4025634d64a927df0d585ad47
Rodrigo-874 <mocrodrigo414@gmail.com>
2026-06-15 10:24:02 +0100

    Add files via upload

---
+++ Exercicio7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Exercicio7
+{
+    static void Main(string[] args)
+    {
+        int nota = 86;
+
+        switch (nota)
+        {
+            case int n when (nota >= 90):
+                Console.WriteLine("A");
+                break;
+
+            case int n when (nota >= 80):
+                Console.WriteLine("B");
+                break;
+
+            case int n when (nota >= 70):
+                Console.WriteLine("C");
+                break;
+
+            case int n when (nota >= 60):
+                Console.WriteLine("D");
+                break;
+
+            case int n when (nota <60):
+                Console.WriteLine("Reprovado");
+                break;
+
+            default:
+                Console.WriteLine("Faltou");
+                break;
+
+        }
+    }
+}
