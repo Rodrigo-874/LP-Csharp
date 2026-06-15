cdad87ab2ca27557568bd0b75244a1b31e6d4e49
Rodrigo-874 <mocrodrigo414@gmail.com>
2026-06-15 10:26:19 +0100

    Add files via upload

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Exercicio3
+{
+    public static void Main(string[] args)
+    {
+        int num1 = 25;
+        int num2 = 29;
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("O número maior é: " + num1);
+        }
+        else
+        {
+            Console.WriteLine("O número maior é: " + num2);
+        }
+        Console.WriteLine("Os números são: " + num1 + " e " + num2);
+    }
+
+
+}
