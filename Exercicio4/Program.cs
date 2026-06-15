08d67fec5144e177b877bcfed5bbd1ca87bab84d
Rodrigo-874 <mocrodrigo414@gmail.com>
2026-06-15 10:25:51 +0100

    Add files via upload

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Exercicio4
+{
+    public static void Main(string[] args)
+    {
+        int idade = 19;
+
+        if (idade < 11) 
+        {
+            Console.WriteLine("És uma criança.");
+        }
+        else if (idade >= 11 && idade < 18) 
+        {
+            Console.WriteLine("És um adolescente.");
+        }
+        else
+        {
+            Console.WriteLine("Já és um adulto.");
+        }
+
+}   }
