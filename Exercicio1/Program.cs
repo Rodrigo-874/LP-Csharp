6fcaac28791100551d1ccb7e1d73e27a705426de
Rodrigo-874 <mocrodrigo414@gmail.com>
2026-06-15 10:27:28 +0100

    Add files via upload

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+    public class Exercicio1
+{ 
+    public static void Main(string[]args)
+    {
+        int num1 = 46;
+        int num2 = 5;
+
+        int soma = num1 + num2;
+        int subtracao = num1 - num2;
+        int multiplicacao = num1 * num2;
+        int divisao = num1 / num2;
+        int resto = num1 % num2;
+        
+        Console.WriteLine("Soma: " + soma);
+        Console.WriteLine("Subtração: " + subtracao);
+        Console.WriteLine("Multiplicação: " + multiplicacao);
+        Console.WriteLine("Divisão: " + divisao);
+        Console.WriteLine("Resto: " + resto);
+
+
+    }
+        
+        
+}
