382dbf658a238c24c49471bf5122719c4d63d39e
Rodrigo-874 <mocrodrigo414@gmail.com>
2026-06-19 09:25:44 +0100

    Add files via upload

---
+++ Exercício11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Exercício11
+{
+    public static void Main(string[] args)
+    {
+        int i = 3;
+
+        while (i <= 30)
+        {
+            Console.WriteLine(i);
+            i += 3;
+        }
+        
+      
+    }
+}
